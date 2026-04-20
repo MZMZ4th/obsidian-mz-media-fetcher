c97d5238f2e40828f2447e4cc5378415e08e311c
MZ <shawncafu6463@gmail.com>
2026-04-21 03:33:26 +0800

    Release 0.3.4

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -10,7 +10,7 @@ import type {
 export const PLUGIN_ID = "mz-media-fetcher";
 export const LEGACY_PLUGIN_ID = "MZ-media-fetcher";
 export const PLUGIN_NAME = "MZ Media Fetcher";
-export const PLUGIN_VERSION = "0.3.3";
+export const PLUGIN_VERSION = "0.3.4";
 export const PLUGIN_DESCRIPTION =
   "Create media and event notes from Bangumi, MobyGames, Bilibili Show, and Showstart.";
 export const HTTP_USER_AGENT = `${PLUGIN_NAME}/${PLUGIN_VERSION} (Obsidian)`;
